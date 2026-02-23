f9387e6bf64063631e2df030487e493d5d5578e5
satyaki <208557303+satyakigh@users.noreply.github.com>
2026-02-23 12:42:22 -0500

    Do not log on tests (#448)

--- src/app/initialize.ts
+++ src/app/initialize.ts
@@ -2,32 +2,34 @@ import { arch, machine, platform, release, type } from 'os';
 import { AwsMetadata, ClientInfo } from '../server/InitParams';
 import { LoggerFactory } from '../telemetry/LoggerFactory';
 import { TelemetryService } from '../telemetry/TelemetryService';
-import { ProcessType, Service, ServiceEnv } from '../utils/Environment';
+import { isTest, ProcessType, Service, ServiceEnv } from '../utils/Environment';
 import { ExtensionName } from '../utils/ExtensionConfig';
 import { Storage } from '../utils/Storage';
 import { toString } from '../utils/String';
 
 export function staticInitialize(ClientInfo?: ClientInfo, AwsMetadata?: AwsMetadata) {
-    // eslint-disable-next-line no-console
-    console.info(
-        toString({
-            Service: Service,
-            Environment: ServiceEnv,
-            Process: ProcessType,
-            Machine: `${type()}-${platform()}-${arch()}-${machine()}-${release()}`,
-            Runtime: `node=${process.versions.node} v8=${process.versions.v8} uv=${process.versions.uv} modules=${process.versions.modules}`,
-            ClientInfo,
-            aws: {
-                clientInfo: AwsMetadata?.clientInfo,
-                telemetryEnabled: AwsMetadata?.telemetryEnabled,
-                logLevel: AwsMetadata?.logLevel,
-                cloudformation: AwsMetadata?.cloudformation,
-                featureFlags: AwsMetadata?.featureFlags,
-                schema: AwsMetadata?.schema,
-            },
-        }),
-        `${ExtensionName} initializing...`,
-    );
+    if (!isTest) {
+        // eslint-disable-next-line no-console
+        console.info(
+            toString({
+                Service: Service,
+                Environment: ServiceEnv,
+                Process: ProcessType,
+                Machine: `${type()}-${platform()}-${arch()}-${machine()}-${release()}`,
+                Runtime: `node=${process.versions.node} v8=${process.versions.v8} uv=${process.versions.uv} modules=${process.versions.modules}`,
+                ClientInfo,
+                aws: {
+                    clientInfo: AwsMetadata?.clientInfo,
+                    telemetryEnabled: AwsMetadata?.telemetryEnabled,
+                    logLevel: AwsMetadata?.logLevel,
+                    cloudformation: AwsMetadata?.cloudformation,
+                    featureFlags: AwsMetadata?.featureFlags,
+                    schema: AwsMetadata?.schema,
+                },
+            }),
+            `${ExtensionName} initializing...`,
+        );
+    }
 
     Storage.initialize(AwsMetadata?.storageDir);
     LoggerFactory.initialize(AwsMetadata?.logLevel);
